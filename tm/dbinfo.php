ca6c6268a522db9d26386b4fd04447046550ff90
Timothy McKenna <mckenna.tim@gmail.com>
2012-09-24 19:46:55 +0000

    reamde

--- tm/dbinfo.php
+++ tm/dbinfo.php
@@ -2,7 +2,7 @@
 // defines database connection data
 define('DB_HOST', 'localhost');
 define('DB_USER', 'pathbost_tim');
-define('DB_PASSWORD', 'nji9ol');
+define('DB_PASSWORD', 'xxxx');
 define('DB_DATABASE', 'pathbost_assess'); 
 // defines the number of visible rows in grid
 ?>
